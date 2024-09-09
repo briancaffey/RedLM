3cca5b6077cdfc5a62bc96d0048d9b644db5fbb8
Brian Caffey <briancaffey@gmail.com>
2024-09-09 05:20:43 +0000

    feat(trt-llm-api): use tensorrt-llm api to do translation, add sample translation for chapter 111

---
+++ hllm/scripts/trtllm_translate.py
@@ -0,0 +1,69 @@
+import os
+import json
+
+from tensorrt_llm import LLM, SamplingParams
+from tensorrt_llm.hlapi import BuildConfig
+
+# models to use
+# Qwen/Qwen2-7B-Instruct
+# baichuan-inc/Baichuan2-13B-Chat
+MODEL = "Qwen/Qwen2-7B-Instruct"
+
+# Version is "original" | "english" | "mandarin"
+VERSION = "original"
+
+bh_to_zh = "把下面的白话写成用简单的现代汉语的句子：\n\n"
+zh_to_en = "Translate the following from Chinese to English:\n\n"
+
+def get_chapter_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=2048)
+build_config = BuildConfig(max_seq_len=4096)
+
+llm = LLM(model=MODEL, build_config=build_config)
+
+
+def translate_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+                print(f"Translating: {file_path}")
+
+                # get paragraphs to translate
+                chapter_data = get_chapter_data(file_path)
+                chapter_paragraphs = chapter_data.get("paragraphs", [])
+                paragraphs = [p["original"] for p in chapter_paragraphs]
+
+                # bai hua to chinese
+                prompts = [f"{bh_to_zh}{p}\n\n" for p in paragraphs]
+                chinese_outputs = llm.generate(prompts, sampling_params)
+                chinese_paragraphs = [output.outputs[0].text for output in chinese_outputs]
+
+                # chinese to english
+                prompts = [f"{zh_to_en}{p}\n\n" for p in chinese_paragraphs]
+                english_outputs = llm.generate(prompts, sampling_params)
+                english_paragraphs = [output.outputs[0].text for output in english_outputs]
+
+                # add chinese and english translations to chapter_data
+                all_paragraphs = [
+                    {**og_dict, "english": en_val, "chinese": cn_val}
+                    for og_dict, en_val, cn_val in zip(chapter_paragraphs, english_paragraphs, chinese_paragraphs)
+                ]
+
+                chapter_data["paragraphs"] = all_paragraphs
+
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    json.dump(chapter_data, file, ensure_ascii=False, indent=4)
+
+                print(f"Translated: {file_path}")
+            break
+
+
+
+directory_to_translate = "data/book"
+translate_directory(directory_to_translate)
+
