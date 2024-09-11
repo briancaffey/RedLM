833a45af9a2895b6b01b10a1fb0aa5ffab5dbec0
Brian Caffey <briancaffey2010@gmail.com>
2024-09-11 16:19:00 -0400

    fix(ui): scroll to bottom after question is answered

--- ui/pages/qa.vue
+++ ui/pages/qa.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="flex flex-col w-xl max-w-xl mx-auto space-y-4">
     <div class="text-center text-3xl">问答</div>
-    <div v-for="(message, index) in store.messages" :key="index" :class="[ message.role === 'assistant' ? ' self-start' : 'self-end', 'rounded']">
-        <div>
-          <p :class="[
-            message.role === 'assistant'
-            ? 'bg-red-700 text-white self-start text-left'
-            : 'bg-white self-end text-right',
-            'max-w-md p-4 rounded-lg'
-          ]">
+    <div
+      v-for="(message, index) in store.messages"
+      :key="index"
+      :class="[ message.role === 'assistant' ? ' self-start' : 'self-end', 'rounded'
+      ]">
+      <div>
+        <p :class="[
+          message.role === 'assistant'
+          ? 'bg-red-700 text-white self-start text-left'
+          : 'bg-white self-end text-right',
+          'max-w-md p-4 rounded-lg'
+        ]">
           {{ message.content }}
         </p>
       </div>
@@ -17,12 +21,12 @@
     <div class="flex flex-col space-y-2">
       <textarea
         v-model="store.query"
-        @keyup.enter="store.sendQuery"
+        @keyup.enter="askQuestion"
         placeholder="写下汝之疑问"
         class="border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <button
-        @click="store.sendQuery"
+        @click="askQuestion"
         :disabled="store.isLoading"
         class="bg-red-700 text-white p-2 rounded disabled:bg-gray-300"
       >
@@ -36,4 +40,9 @@
 import { useQAStore } from '@/stores/qa'
 
 const store = useQAStore()
+
+const askQuestion = async () => {
+  await store.sendQuery();
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
 </script>
